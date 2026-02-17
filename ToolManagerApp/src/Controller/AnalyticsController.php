b47a36efd23e8f850d56409771f961182974b844
Aswan1901 <aswan.josephmathieu1999@gmail.com>
2026-02-17 18:32:59 +0100

    sort analytics

--- ToolManagerApp/src/Controller/AnalyticsController.php
+++ ToolManagerApp/src/Controller/AnalyticsController.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 
 
@@ -17,11 +18,10 @@ final class AnalyticsController extends AbstractController
     {
     }
 
-    #[Route('/', name: 'app_analytics')]
-    public function analytics(): JsonResponse
+    #[Route('/department-costs', name: 'app_analytics')]
+    public function analytics(Request $request): JsonResponse
     {
         $dataAnalytics=[];
-
         $summary = [
             "total_company_cost" => 0,
             "department_count" => 0,
@@ -37,7 +37,7 @@ final class AnalyticsController extends AbstractController
 
             if (!isset($dataAnalytics[$department])){
                 $dataAnalytics[$department]=[
-                    "totalCost"=>0,
+                    "totalCost"=> 0,
                     "totalCount"=>0,
                     "totalUsers"=>0,
                     "averageCost"=>0,
@@ -45,35 +45,44 @@ final class AnalyticsController extends AbstractController
                 ];
             }
 
-            $dataAnalytics[$department]["totalCost"] += round($toolCost, 2);
+            $dataAnalytics[$department]["totalCost"] = round($dataAnalytics[$department]["totalCost"] + $toolCost, 2);
             $dataAnalytics[$department]["totalCount"]++;
             $dataAnalytics[$department]["totalUsers"] += $users;
 
         }
+
+        //la somme totale de tous les départements
         $totalDepartmentsCost = array_sum(
             array_column($dataAnalytics, "totalCost")
         );
 
-        $summary["total_company_cost"] = $totalDepartmentsCost;
+        $summary["total_company_cost"] = round($totalDepartmentsCost,2) ;
         $maxCost = 0;
 
-            foreach($dataAnalytics as $department => $stats){
+        foreach($dataAnalytics as $department => $stats){
 
-                $averageCost  = $stats["totalCost"] / $stats["totalCount"];
-                $percentageCost = ($stats["totalCost"] / $totalDepartmentsCost) * 100;
+            $averageCost  = $stats["totalCost"] / $stats["totalCount"];
+            $percentageCost = ($stats["totalCost"] / $totalDepartmentsCost) * 100;
 
-                $dataAnalytics[$department]["averageCost"] = round($averageCost, 2);
-                $dataAnalytics[$department]["percentageCost"] = round($percentageCost, 2);
+            $dataAnalytics[$department]["averageCost"] = round($averageCost, 2);
+            $dataAnalytics[$department]["percentageCost"] = round($percentageCost, 2);
 
-                if ($stats["totalCost"] > $maxCost)
-                {
-                    $maxCost = $stats["totalCost"];
-                    $summary["most_expensive_department"] = $department;
-                }
-
+            if ($stats["totalCost"] > $maxCost)
+            {
+                $maxCost = $stats["totalCost"];
+                $summary["most_expensive_department"] = $department;
             }
+        }
 
-            $summary["department_count"] = count($dataAnalytics);
+        $summary["department_count"] = count($dataAnalytics);
+        $sortByTotalCost = $request->query->get('sortByTotalCost');
+
+        if ($sortByTotalCost === 'asc') {
+            uasort($dataAnalytics, fn($a, $b) => $a['totalCost'] <=> $b['totalCost']);
+        } elseif ($sortByTotalCost === 'desc') {
+            uasort($dataAnalytics, fn($a, $b) => $b['totalCost'] <=> $a['totalCost']);
+        }
+
         return $this->json([
             "data analytics" => $dataAnalytics,
             "summary" => $summary,
